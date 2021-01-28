27f66098b72c05c9cb5b528e999aa016522629ef
mqd <1045723308@qq.com>
2021-01-28 13:11:22 +0800

    chat

---
+++ ch18/chat_clnt.cpp
@@ -0,0 +1,153 @@
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+/*
+ * 多人聊天客户端
+ * 原理:
+ *  1. 开两个线程, 一个负责读, 一个负责写
+ *  2. 好像没了
+ */
+
+#define BUF_SIZE 1024
+#define CLNT_MAX_NUM 100
+#define NAME_MAX_LEN 30
+
+void error_handling(char* message);
+void succ(char* message);
+void init_sock(int*, char*, char*);
+int read_msg(int, char*, int);
+void* read_from_server(void*);
+void* write_to_server(void*);
+
+pthread_mutex_t mutx_read, mutx_write;
+int main(int argc, char** argv) {
+    char buf[3][20] = {"chat_clnt", "121.37.129.143", "9190"};
+    if (argc != 3) {
+        // printf("Usage: %s <IP> <port>\n", argv[0]);
+        // exit(1);
+        argv[1] = buf[1];
+        argv[2] = buf[2];
+    }
+    int sock;
+    struct sockaddr_in serv_addr;
+    init_sock(&sock, argv[1], argv[2]);
+    pthread_t tr_id1, tr_id2;
+    pthread_mutex_init(&mutx_read, NULL);
+    pthread_mutex_init(&mutx_write, NULL);
+    pthread_create(&tr_id1, NULL, read_from_server, (void*)&sock);
+    pthread_create(&tr_id2, NULL, write_to_server, (void*)&sock);
+    // pthread_join(tr_id1, NULL);
+    pthread_join(tr_id2, NULL);
+    pthread_mutex_destroy(&mutx_read);
+    pthread_mutex_destroy(&mutx_write);
+    close(sock);
+}
+
+void error_handling(char* message) {
+    fprintf(stderr, "%s error\n", message);
+    exit(1);
+}
+void succ(char* message) {
+    fprintf(stdout, "%s successed\n", message);
+}
+
+void init_sock(int* sock, char* ip, char* port) {
+    struct sockaddr_in serv_addr;
+    *sock = socket(PF_INET, SOCK_STREAM, 0);
+    if (*sock == -1)
+        error_handling((char*)"socket()");
+    else
+        succ((char*)"socket()");
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = PF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(ip);
+    serv_addr.sin_port = htons(atoi(port));
+    if (connect(*sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+        error_handling((char*)"connect");
+    else
+        succ((char*)"connect");
+}
+
+void* read_from_server(void* args) {
+    int sock = *((int*)args);
+    int name_len, str_len;
+    char name[NAME_MAX_LEN], buf[BUF_SIZE];
+    while (1) {
+        pthread_mutex_lock(&mutx_read);
+        int sig = read(sock, &name_len, sizeof(name_len));
+        if (sig == -1)
+            error_handling((char*)"read()");
+        if (name_len == -1) {
+            pthread_mutex_unlock(&mutx_read);
+            break;
+        }
+        read_msg(sock, name, name_len);
+        read(sock, &str_len, sizeof(str_len));
+        read_msg(sock, buf, str_len);
+        pthread_mutex_unlock(&mutx_read);
+        fprintf(stdout, "%*s: %s\n", 15, name, buf);
+    }
+    close(sock);
+    return NULL;
+}
+
+int read_msg(int sock, char* buf, int str_len) {
+    int read_len = 0;
+    while (read_len < str_len) {
+        read_len += read(sock, buf + read_len, str_len - read_len);
+        if (read_len < 0)
+            return -1;
+    }
+    buf[read_len] = 0;
+    return read_len;
+}
+
+void* write_to_server(void* args) {
+    int sock = *((int*)args);
+    int name_len, str_len;
+    char name[NAME_MAX_LEN], buf[BUF_SIZE], *str;
+
+    fprintf(stdout, "----多人聊天极简版-----\n请输入你的名字或昵称: ");
+    fgets(name, NAME_MAX_LEN, stdin);
+    name_len = strlen(name) - 1;
+    name[name_len] = 0;
+    pthread_mutex_lock(&mutx_write);
+    write(sock, &name_len, sizeof(name_len));
+    write(sock, name, name_len);
+    str = "加入聊天室";
+    str_len = strlen(str);
+    write(sock, &str_len, sizeof(str_len));
+    write(sock, str, str_len);
+    pthread_mutex_unlock(&mutx_write);
+    fprintf(stdout,
+            "开始多人聊天聊天, 可以开始输入, 按回车发送, 输入q结束程序\n");
+    while (1) {
+        fgets(buf, BUF_SIZE, stdin);
+        str_len = strlen(buf) - 1;
+        buf[str_len] = 0;
+        pthread_mutex_lock(&mutx_write);
+        if (str_len == 1 && buf[0] == 'q') {
+            char* ptr = "退出聊天室";
+            str_len = strlen(ptr);
+            write(sock, &str_len, sizeof(str_len));
+            write(sock, ptr, str_len);
+            str_len = -1;
+            write(sock, &str_len, sizeof(str_len));
+            pthread_mutex_unlock(&mutx_write);
+            break;
+        }
+        // fprintf(stdout, "%d %s: %s\n", name_len, name, buf);
+        write(sock, &str_len, sizeof(str_len));
+        write(sock, buf, str_len);
+        pthread_mutex_unlock(&mutx_write);
+    }
+    fprintf(stdout, "程序结束, 欢迎下次再来\n");
+    // close(sock);
+    return NULL;
+}
---
+++ ch18/chat_server.cpp
@@ -0,0 +1,126 @@
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+/*
+ * 多人聊天服务器
+ * 原理:
+ *  1. 多线程处理每一个连接
+ *  2. 一人发消息, 往所有人都发送一遍
+ *  3. mutex解决临界区问题
+ *
+ */
+
+#define CLNT_MAX_NUM 100
+#define NAME_MAX_LEN 30
+void error_handling(char*);               //打印错误信息
+void succ(char*);                         //打印成功信息
+void sock_init(int*, sockaddr_in*, int);  //初始化套接字
+void* handle_clnt(void*);
+
+int clnt_cnt = 0, socks[CLNT_MAX_NUM];
+pthread_mutex_t mutx;
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        printf("Usage: %s <port>", argv[0]);
+        exit(1);
+    }
+    int serv_sock, clnt_sock;
+    struct sockaddr_in serv_addr, clnt_addr;
+    socklen_t sock_len;
+    pthread_t tr_id;
+    pthread_mutex_init(&mutx, NULL);
+    sock_init(&serv_sock, &serv_addr, atoi(argv[1]));
+    while (1) {
+        if ((clnt_sock = accept(serv_sock, (sockaddr*)&clnt_addr, &sock_len)) ==
+            -1)
+            error_handling((char*)"accept()");
+        else
+            succ((char*)"accept()");
+        socks[clnt_cnt++] = clnt_sock;
+        pthread_create(&tr_id, NULL, handle_clnt,
+                       (void*)(socks + clnt_cnt - 1));
+        pthread_detach(tr_id);
+    }
+    pthread_mutex_destroy(&mutx);
+}
+
+void error_handling(char* mess) {
+    fprintf(stderr, "pid %d %s error\n", getpid(), mess);
+    exit(1);
+}
+void succ(char* mess) {
+    fprintf(stdout, "pid %d %s successed\n", getpid(), mess);
+}
+
+void sock_init(int* sock, sockaddr_in* addr, int port) {
+    *sock = socket(PF_INET, SOCK_STREAM, 0);
+    if (*sock == -1)
+        error_handling((char*)"socket()");
+    else
+        succ((char*)"socket()");
+    memset(addr, 0, sizeof(sockaddr));
+    addr->sin_family = PF_INET;
+    addr->sin_addr.s_addr = htonl(INADDR_ANY);
+    addr->sin_port = htons(port);
+
+    if (bind(*sock, (sockaddr*)addr, sizeof(sockaddr)) == -1)
+        error_handling((char*)"bind()");
+    else
+        succ((char*)"bind()");
+
+    if (listen(*sock, 5) == -1)
+        error_handling((char*)"listen()");
+    else
+        succ((char*)"listen()");
+}
+
+void* handle_clnt(void* args) {
+    int sock = *((int*)args);
+    int name_len, str_len;
+    int i;
+    char buf[BUFSIZ], name[NAME_MAX_LEN];
+
+    // printf("%d %d\n",sock,name_len);
+    read(sock, &name_len, sizeof(name_len));
+    pthread_mutex_lock(&mutx);
+    // printf("%d %d\n",sock,name_len);
+    read(sock, name, name_len);
+    name[name_len] = 0;
+    pthread_mutex_unlock(&mutx);
+    fprintf(stdout, "----------\n");
+    while (1) {
+        read(sock, &str_len, sizeof(str_len));
+        pthread_mutex_lock(&mutx);
+        read(sock, buf, str_len);
+        if (str_len == -1) {
+            pthread_mutex_unlock(&mutx);
+            fprintf(stderr, "%s 用户退出\n", name);
+            break;
+        }
+        pthread_mutex_unlock(&mutx);
+        buf[str_len] = 0;
+        printf("--%s: %s\n", name, buf);
+        for (i = 0; i < clnt_cnt; i++) {
+            pthread_mutex_lock(&mutx);
+            write(socks[i], &name_len, sizeof(name_len));
+            write(socks[i], name, name_len);
+            write(socks[i], &str_len, sizeof(str_len));
+            write(socks[i], buf, str_len);
+            pthread_mutex_unlock(&mutx);
+        }
+    }
+    i = 0;
+    while (socks[i] != sock)
+        i++;
+    clnt_cnt--;
+    while (i < clnt_cnt)
+        socks[i] = socks[i + 1];
+    close(sock);
+}
